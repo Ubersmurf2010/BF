9d1ba474877d3a54a0d2d83f11685cdb025760bb
Kazantsev Gera <113335397+Ubersmurf2010@users.noreply.github.com>
2023-09-02 14:10:20 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4444',
+    baseURL: 'https://ubersmurf-notes.vercel.app',
 });
 
 //функция посредник, которая при каждом запросе проверяет авторизацию по токену
